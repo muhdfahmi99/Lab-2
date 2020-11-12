50dc0c3b083bb6760127c1a503a80de066a5cea0
Md Fahmi <muhdfahmi17.sf@gmail.com>
2020-11-12 16:34:17 +0000

    preparing for 2.3

--- 2.2.c
+++ 2.2.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/wait.h>
 
 int main(int arg,char **argv){
 int pid;
@@ -8,8 +9,10 @@ switch (pid = fork()){
 case 0: printf("I am the child process: pid=%d \n",getpid());
 break;
 default: printf("I am the parent process: pid=%d \n, child pid=%d \n",getpid(), pid);
+wait(NULL);
 break;
 case -1: perror("fork");
+exit(1);
 }
 exit(0);
 }
